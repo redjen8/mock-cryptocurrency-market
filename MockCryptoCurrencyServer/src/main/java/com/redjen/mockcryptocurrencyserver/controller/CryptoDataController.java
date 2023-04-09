b60b71b44e9d9c70525a4b69a3a68fcd7812e930
redjen8 <seokwoo2525@gmail.com>
2023-04-09 17:52:44 +0900

    Feat: api path angular proxy로 연결

--- MockCryptoCurrencyServer/src/main/java/com/redjen/mockcryptocurrencyserver/controller/CryptoDataController.java
+++ MockCryptoCurrencyServer/src/main/java/com/redjen/mockcryptocurrencyserver/controller/CryptoDataController.java
@@ -6,12 +6,14 @@ import java.time.Instant;
 import com.redjen.mockcryptocurrencyserver.service.RandomService;
 import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.redjen.mockcryptocurrencyserver.dto.LiveCryptoDataDto;
 import reactor.core.publisher.Flux;
 
 @RestController
+@RequestMapping("/api")
 public class CryptoDataController {
 
     private final RandomService randomService;
