6b05ef0c2d703ef355f8321ed42d1c7c45e93ded
redjen8 <seokwoo2525@gmail.com>
2023-04-09 17:30:53 +0900

    Refact: 사용하지 않는 import 문 제거

--- MockCryptoCurrencyServer/src/main/java/com/redjen/mockcryptocurrencyserver/controller/CryptoDataController.java
+++ MockCryptoCurrencyServer/src/main/java/com/redjen/mockcryptocurrencyserver/controller/CryptoDataController.java
@@ -4,8 +4,6 @@ import java.time.Duration;
 import java.time.Instant;
 
 import com.redjen.mockcryptocurrencyserver.service.RandomService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
